b62134d8646b5862e28dfd79ba6ed67c3ef883f0
chen-fang <eatappleonmars@gmail.com>
2024-04-13 10:48:25 -0700

    Remove FilterInspector. Will push dedicated commit to show how to debug or print regiestered filters

--- src/main/java/org/example/springlifecycle/controllers/SayHelloController.java
+++ src/main/java/org/example/springlifecycle/controllers/SayHelloController.java
@@ -1,6 +1,5 @@
 package org.example.springlifecycle.controllers;
 
-import org.example.springlifecycle.FilterInspector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SayHelloController {
 
-    private final FilterInspector filterInspector;
-
-    public SayHelloController(FilterInspector filterInspector){
-        this.filterInspector = filterInspector;
+    public SayHelloController(){
     }
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        filterInspector.printRegisteredFilters();
         return String.format("Hello %s!", name);
     }
 }
